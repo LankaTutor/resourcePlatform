c3791cfba4eab0efa5c73a59e15fbdd485d47d37
priyansiva <priyansiva1995@gmail.com>
2020-10-06 20:00:51 +0530

    article

--- src/Resources/Article/Components/CardArticle.js
+++ src/Resources/Article/Components/CardArticle.js
@@ -7,6 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles({
   root: {
@@ -17,31 +18,31 @@ const useStyles = makeStyles({
   },
 });
 
-function CardArticle({ subject, description, img, author }) {
+function CardArticle({ title, description, img, author, avatar }) {
   const classes = useStyles();
 
   return (
     <div>
       <Card className={classes.root}>
         <CardActionArea>
-          <CardMedia className={classes.media} image={img} title={subject} />
+          <CardMedia className={classes.media} image={img} title={title} />
+          <CardActions>
+            <Button size="small" color="primary">
+              <Avatar alt={author} src={avatar} />
+            </Button>
+            <Button size="small" color="primary">
+              {author}
+            </Button>
+          </CardActions>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {subject}
+            <Typography gutterBottom variant="body1" component="h2">
+              {title}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              There are sub units and video lectures available for {subject}
+              {description}
             </Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Enroll Now
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
       </Card>
     </div>
   );
