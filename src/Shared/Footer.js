f51aeeebc894ed022794e535c191281edf702572
Anojiya <anojiya8@gmail.com>
2020-10-10 01:07:02 +0530

    footer modified

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -3,39 +3,42 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="footer-container">
-      <div className="footer-container__text">
-        <div>
-          <h1 className="footer-container__logo center pt-2">Tutor Lanka</h1>
-          <p className="footer-container__copyright">
-            <i className="fa fa-copyright center pt-1 mr-1"></i>
-            copyright 2020. All Rights Reserved.
-          </p>
-        </div>
-        <div className="footer-container__catogory">
-          <ul>
-            <li>
-              <i className="fa fa-info-circle mr-1 color1"></i>
-              <a href="#"> About Us</a>
-            </li>
-            <li>
-              <i className="fa fa-list mr-1 color1"></i>
-              <a href="#">Terms of Use</a>
-            </li>
-            <li>
-              <i className="fa fa-lock mr-1 color1"></i>
-              <a href="#">Privacy Policy</a>
-            </li>
-            <li>
-              <i className="fa fa-question-circle mr-1 color1"></i>
-              <a href="#">FAQ's</a>
-            </li>
-            <li>
-              <i className="fa fa-map-marker mr-1 color1"></i>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+        <div className="footer-container__catogory row">
+          <div className="col-lg-1">
+            </div>
+            <div className="col-lg-1">
+              <ul className="list-group">
+                <li><a href="/login">Login</a></li>
+                <li><a href="/signup">Join Us</a></li>
+              </ul>
+            </div>
+            <div className="col-lg-1">
+              <ul className="list-group">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+            <div className="col-lg-1">
+              <ul className="list-group">
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms</a></li>
+              </ul>
+            </div>
+            <div className="col-lg-1">
+              <ul className="list-group">
+                <li><a href="#">FAQ's</a></li>
+              </ul>
+            </div>
+            <div className="col-lg-4">
+            </div>
+            <div className="col-lg-3">
+              <a className="footer-logo" href="/">
+              <img src="https://i.ya-webdesign.com/images/libro-vector-book-5.png" width="40" height="40" class="" alt="" />
+                Lanka Tutor </a><p className="footer-container__copyright">
+              <i className="fa fa-copyright pt-1 mr-1"></i>
+              copyright 2020. All Rights Reserved.</p>
+            </div>
+          </div>
     </div>
   );
 }
