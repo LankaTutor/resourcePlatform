a247fcd749db61e86938fb07ee30c080493ff27f
Anojiya <anojiya8@gmail.com>
2020-10-10 20:25:14 +0530

    admin dashboard modified

--- src/Resources/Article/Components/Articles.js
+++ src/Resources/Article/Components/Articles.js
@@ -71,6 +71,39 @@ function Articles() {
             />
           </Paper>
         </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <CardArticle
+              title="Biology of Gene"
+              author="Yathurshan"
+              avatar="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
+              description="In biology, a gene is a sequence of nucleotides in DNA or RNA that encodes the synthesis of a gene product, either RNA or protein. During gene expression, the DNA is first copied into RNA. "
+              img="https://medlineplus.gov/images/PX00006G_PRESENTATION.jpeg"
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <CardArticle
+              title="Trignometry - A Deep Dive"
+              author="Dilushkumar"
+              avatar="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
+              description="Trigonometry is a branch of mathematics that studies relationships between side lengths and angles of triangles"
+              img="https://i.ytimg.com/vi/PUB0TaZ7bhA/hqdefault.jpg"
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <CardArticle
+              title="Chemistry of Future World"
+              author="Mayurathan"
+              avatar="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
+              description="Quantum chemistry, also called molecular quantum mechanics, is a branch of chemistry focused on the application of quantum mechanics in physical models and experiments of chemical systems."
+              img="https://d1aueex22ha5si.cloudfront.net/Conference/902/Highlight/14.%20Quantum%20Chemistry-1584599077703.png"
+            />
+          </Paper>
+        </Grid>
       </Grid>
     </div>
   );
